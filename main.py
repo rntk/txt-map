8d00116a16a7386209f928d1b0f5a1ac7771522c
rntk <kvrenat@gmail.com>
2026-01-26 20:28:13 +0100

    remove unnecessary handlers

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from handlers import sgr_topics_handler, themed_post_handler, clustered_post_handler, topics_handler, themed_topic_handler, mindmap_handler, insides_handler, submission_handler
+from handlers import topics_handler, themed_topic_handler, submission_handler
 from pymongo import MongoClient
 from lib.storage.posts import PostsStorage
 from lib.storage.submissions import SubmissionsStorage
-from lib.llm.llamacpp import LLamaCPP
 
 app = FastAPI(title="My FastAPI App", description="A simple FastAPI application with separate handlers")
 
 app.mount("/static", StaticFiles(directory="frontend/build/static"), name="static")
 
-app.include_router(themed_post_handler.router, prefix="/api")
-app.include_router(clustered_post_handler.router, prefix="/api")
-app.include_router(sgr_topics_handler.router, prefix="/api")
 app.include_router(topics_handler.router, prefix="/api")
 app.include_router(themed_topic_handler.router, prefix="/api")
-app.include_router(mindmap_handler.router, prefix="/api")
-app.include_router(insides_handler.router, prefix="/api")
 app.include_router(submission_handler.router, prefix="/api")
 
 import os
 mongodb_url = os.getenv("MONGODB_URL", "mongodb://localhost:8765/")
-llamacpp_url = os.getenv("LLAMACPP_URL", "http://localhost:8989")
-token = os.getenv("TOKEN")
 
 print(f"MONGODB_URL: {mongodb_url}")
-print(f"LLAMACPP_URL: {llamacpp_url}")
-if token:
-    print(f"TOKEN: {token[:10]}...")
-else:
-    print("TOKEN: not set")
 
 client = MongoClient(mongodb_url)
 posts_storage = PostsStorage(client["rss"])
@@ -39,7 +26,6 @@ submissions_storage = SubmissionsStorage(client["rss"])
 submissions_storage.prepare()
 app.state.posts_storage = posts_storage
 app.state.submissions_storage = submissions_storage
-app.state.llamacpp = LLamaCPP(host=llamacpp_url, token=token)
 
 @app.get("/page/themed-post")
 def serve_themed_post_page():
